a2e2d045ee431e57eab36eff3018b022aa961e86
Daniel Gebhart <daniel.gebhart@hotmail.com>
2024-10-14 14:02:30 +0200

    Add comments and initialize TcpServer
    Added a comment with a GitHub repository link for reference. Included additional comments explaining that user classes, cards, etc., are placeholders and methods are deprecated, to be completed for the final submission. Initialized and started a TcpServer instance with "localhost" and port 10001.

--- MonsterCardGame/Program.cs
+++ MonsterCardGame/Program.cs
@@ -8,5 +8,11 @@ using System.Security.Cryptography;
 using System.Runtime.InteropServices.Marshalling;
 using System.Reflection.Metadata;
 
+//*link zu github repository*: https://github.com/LeafOfBread/MTCG-Projekt/tree/master
+
+/*user classes, cards etc. sind basically platzhalter und somit incomplete, methoden 
+innerhalb der klassen sind deprecated und werden erst fuer die volle abgabe vervollständigt,
+relevant ist somit also nur die tcpserver klasse*/
+
         TcpServer tcpServer = new TcpServer();
         tcpServer.Start("localhost", 10001);
