e568ebd3419efc537dae882922fcbc7925b2fd40
Daniel Gebhart <daniel.gebhart@hotmail.com>
2025-01-02 14:43:08 +0100

    Refactor to use Host for app setup and dependency injection PostgreSQL database tables defined and database conection established, no errors
    Updated MonsterCardGame.csproj:
    - Added Microsoft.Extensions.Hosting 9.0.0
    - Removed System.Text.Json 8.0.4, added System.Text.Json 9.0.0
    - Added Npgsql.EntityFrameworkCore.PostgreSQL 9.0.2
    
    Modified Program.cs:
    - Added using directives for DI, config, and logging
    - Refactored main entry point to use Host class
    
    Added Startup class:
    - Configures services and logging
    - Sets up ApplicationDbContext with PostgreSQL

--- MonsterCardGame/Program.cs
+++ MonsterCardGame/Program.cs
@@ -7,12 +7,46 @@ using System.Net;
 using System.Security.Cryptography;
 using System.Runtime.InteropServices.Marshalling;
 using System.Reflection.Metadata;
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-//*link zu github repository*: https://github.com/LeafOfBread/MTCG-Projekt/tree/master
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                // Configure services here
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseNpgsql(hostContext.Configuration.GetConnectionString("DefaultConnection")));
+            });
+}
 
-/*user classes, cards etc. sind basically platzhalter und somit incomplete, methoden 
-innerhalb der klassen sind deprecated und werden erst fuer die volle abgabe vervollständigt,
-relevant ist somit also nur die tcpserver klasse*/
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+        //logging
+        services.AddLogging(builder => builder.AddConsole());
+    }
+}
 
-        TcpServer tcpServer = new TcpServer();
-        tcpServer.Start("localhost", 10001);
+//TcpServer tcpServer = new TcpServer();
+ //       tcpServer.Start("localhost", 10001);
+
